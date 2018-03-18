c613c96caa22b112016218c02dc7c3e7e959c7f1
robin <adawithleon@sina.com>
2018-03-18 15:39:27 +0800

    [CRM][Misc]初始化 CRM Cutomer模块
    [Misc]
    初始化CRM系统及其Cutomer模块

---
+++ crm/src/main/java/com/robin/crm/service/CustomerService.java
@@ -0,0 +1,37 @@
+package com.robin.crm.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.robin.crm.domain.Customer;
+
+/**  
+ * ClassName:CrmService <br/>  
+ * Function:  <br/>  
+ * Date:     2018年3月18日 下午3:00:42 <br/>       
+ */
+@Component
+@Transactional
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON}) // 指定参数的数据传输格式
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON}) // 指定返回值的数据传输格式
+public interface CustomerService {
+    
+    
+    //@GET
+    //@Path("/{id}/findById")
+    //public Customer findById(@PathParam("id")Long id);
+    
+    @GET
+    @Path("/test")
+    public void test();
+    
+    
+}
+  
---
+++ crm/src/main/java/com/robin/crm/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,28 @@
+package com.robin.crm.service.impl;
+
+import com.robin.crm.domain.Customer;
+import com.robin.crm.service.CustomerService;
+
+/**  
+ * ClassName:CrmServiceImpl <br/>  
+ * Function:  <br/>  
+ * Date:     2018年3月18日 下午3:10:12 <br/>       
+ */
+public class CustomerServiceImpl implements CustomerService {
+
+    //@Override
+    public Customer findById(Long id) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setUsername("zs");
+        System.out.println("This is findById...");
+        return customer;
+    }
+
+    @Override
+    public void test() {
+       System.out.println("This is Test");
+    }
+
+}
+  
